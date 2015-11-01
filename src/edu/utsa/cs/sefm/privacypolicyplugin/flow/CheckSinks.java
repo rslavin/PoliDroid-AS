861927d47d2e34288ea33fc2021b6d017ae2c83c
Rocky Slavin <rocky.slavin@gmail.com>
2015-11-01 14:39:07 -0600

    added sink checking to menu

---
+++ src/edu/utsa/cs/sefm/privacypolicyplugin/flow/CheckSinks.java
@@ -0,0 +1,13 @@
+package edu.utsa.cs.sefm.privacypolicyplugin.flow;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Rocky on 11/1/2015.
+ */
+public class CheckSinks extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
