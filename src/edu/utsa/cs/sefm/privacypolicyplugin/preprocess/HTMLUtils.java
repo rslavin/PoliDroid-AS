9c665d9bb18051f31bdcb2904f9235f5a586ebaf
Rocky Slavin <rocky.slavin@gmail.com>
2015-11-19 22:08:16 -0600

    added lib

--- src/edu/utsa/cs/sefm/privacypolicyplugin/preprocess/HTMLUtils.java
+++ src/edu/utsa/cs/sefm/privacypolicyplugin/preprocess/HTMLUtils.java
@@ -5,12 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 /**
  * Created by Mitra on 11/14/2015.
