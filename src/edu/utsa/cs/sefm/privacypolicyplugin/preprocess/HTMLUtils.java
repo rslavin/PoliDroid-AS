5f5f3bebe3e9f23e771ade384d94924748306f11
pfv610 <pfv610@my.utsa.edu>
2015-11-19 23:07:59 -0600

    Some changes to HTMLUtils. Now it can parse txt files and HTML files.

--- src/edu/utsa/cs/sefm/privacypolicyplugin/preprocess/HTMLUtils.java
+++ src/edu/utsa/cs/sefm/privacypolicyplugin/preprocess/HTMLUtils.java
@@ -22,13 +22,16 @@ public class HTMLUtils {
             BufferedReader br = new BufferedReader(new InputStreamReader(policy.getInputStream(),"UTF-8"));
             String line;
             while((line = br.readLine()) != null)
-                documentHtml +=  line;
+                documentHtml +=  line + "\n";
             br.close();
         } catch (Exception e) {
             System.out.println("Failed to read " + policy.getName() + ": " + e.toString());
             return null;
         }
-
+        if (policy.getName().endsWith(".txt")) {
+            System.out.println("file ends with txt");
+            return documentHtml;
+        }
         Document htmlDoc = Jsoup.parse(documentHtml);
         String charset = "UTF-8";
         try {
@@ -37,10 +40,12 @@ public class HTMLUtils {
         }
 
         if (charset.equals("UTF-8")) {
-            String doc = Jsoup.parse(documentHtml.replaceAll("(?i)<br[^>]*>", "br2n")).text();
-            return doc.replaceAll("br2n", "\n");
+            htmlDoc.select("br").append("\\n");
+            htmlDoc.select("p").prepend("\\n\\n");
+            return (htmlDoc.text().replace("\\n", "\n")).replace("\n\n", "\n");
         }
-            System.out.println("Failed to read " + policy.getName());
+
+           System.out.println("Failed to read " + policy.getName());
             return null;
     }
 }
